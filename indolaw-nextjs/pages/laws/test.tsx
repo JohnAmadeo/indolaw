0e3ccc5d1f4f22505afbfe4fc969cf7cbd1826b9
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2020-11-28 20:19:20 -0800

    Render 1st 10 pages of Omnibus as HTML
    Warning: the JS code is quick & dirty; will need to be cleaned up

--- indolaw-nextjs/pages/laws/test.tsx
+++ indolaw-nextjs/pages/laws/test.tsx
@@ -7,36 +7,61 @@ import { GetStaticProps } from "next";
 
 export default function Test(props: {
   data: {
-    contentHtml: string;
+    law: any;
   };
 }) {
   return (
+    <div style={{ margin: "0 124px" }}>
+      <h1>UNDANG UNDANG REPUBLIK INDONESIA TENTANG CIPTA KERJA</h1>
+      {render(props.data.law, 1)}
+    </div>
+  );
+}
+
+function render(structure: any, depth: number) {
+  return (
     <>
-      <h1>Test Page for rendering Markdown</h1>
-      <div dangerouslySetInnerHTML={{ __html: props.data.contentHtml }}></div>
+      {structure.map((childStructure: any) => {
+        if (childStructure instanceof Array) {
+          return render(childStructure, depth + 1);
+        } else {
+          return renderPrimitive(childStructure, depth);
+        }
+      })}
     </>
   );
 }
 
+function renderPrimitive(structure: any, depth: number) {
+  const divStyle = {
+    marginLeft: `${depth * 32}px`,
+    marginBottom: "12px",
+    fontSize: "20px",
+  };
+
+  let textStyle = {};
+  if (
+    structure.type.toLowerCase().includes("number") ||
+    structure.type.toLowerCase().includes("title")
+  ) {
+    textStyle = { fontWeight: "bold" };
+  }
+
+  return (
+    <div style={divStyle}>
+      <p style={textStyle}>{structure.text}</p>
+    </div>
+  );
+}
+
 export const getStaticProps: GetStaticProps = async () => {
-  const markdownDirectory = "";
-  const fullPath = path.join(markdownDirectory, `pre-rendering.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
+  const filePath = "omnibus_law_m1_hand_modified.json";
+  const file = fs.readFileSync(filePath, "utf8");
 
   return {
     props: {
       data: {
-        contentHtml,
-        ...matterResult.data,
+        law: JSON.parse(file),
       },
     },
   };
