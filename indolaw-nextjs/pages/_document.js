bc6fb3a9c3f505ee4004fdbabd054b09c989142e
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2020-12-11 21:08:28 -0800

    [Table of Contents 6/x] Import Merriweather & Merriweather Sans font
    Google Fonts: https://fonts.google.com/?query=merriweather&preview.text=Bab%20I:%20Ketentuan%20Umum&preview.text_type=custom
    
    Tutorial: https://johnny.am/blog/n2-adding-google-fonts-to-nextjs-project
    
    NextJS docs: https://nextjs.org/docs/advanced-features/custom-document
    
    Google Fonts API docs: https://developers.google.com/fonts/docs/getting_started#use_font-display

---
+++ indolaw-nextjs/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Merriweather|Merriweather+Sans"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
