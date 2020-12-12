290de9f035100e71514740154f801290af2d872f
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2020-12-11 21:12:39 -0800

    [Table of Contents 8/x] Add expander & click-to-nav

--- indolaw-nextjs/components/TableOfContents.tsx
+++ indolaw-nextjs/components/TableOfContents.tsx
@@ -1,27 +1,81 @@
-import { CSSProperties } from "react";
+import { CSSProperties, Props, ReactNode, useState } from "react";
 import { Structure, Complex, Primitive } from "utils/grammar";
+import Link from "next/link";
 
 /*
  * TODO(johnamadeo)
- * 2. Collapsible (can hide all child structures)
  * 3. Linking
  */
 
 export default function TableOfContents(props: { law: Complex }): JSX.Element {
-  return renderUndangUndang(props.law);
+  return (
+    <>
+      {props.law.children.map((child) => (
+        <TableOfContentsGroup structure={child} depth={0} />
+      ))}
+    </>
+  );
 }
 
-function renderStructure(
+function TableOfContentsGroup(props: {
+  structure: Complex | Primitive;
+  depth: number;
+}): JSX.Element {
+  const { structure, depth } = props;
+  const [isChildrenVisible, setIsChildrenVisible] = useState(false);
+
+  const children = getChildren(structure, depth + 1);
+
+  const labelText = getLabel(structure);
+  if (labelText === null) {
+    return <></>;
+  }
+
+  const labelLink =
+    "id" in structure && structure.id !== "" ? (
+      <Link href={`/laws/test#${structure.id}`}>{labelText}</Link>
+    ) : (
+      labelText
+    );
+
+  const expander =
+    children !== null ? (
+      <span onClick={() => setIsChildrenVisible(!isChildrenVisible)}>
+        [{isChildrenVisible ? "-" : "+"}]
+      </span>
+    ) : null;
+
+  return (
+    <>
+      <p
+        style={{
+          marginLeft: `${depth * 14}px`,
+          padding: "6px 4px",
+        }}
+      >
+        {expander} {labelLink}
+      </p>
+      {isChildrenVisible && children}
+    </>
+  );
+}
+
+function getChildren(
   structure: Complex | Primitive,
   depth: number
-): JSX.Element {
+): JSX.Element | null {
   switch (structure.type) {
     case Structure.BAB:
     case Structure.BAGIAN:
     case Structure.PARAGRAF:
-      return renderStructuresWithTitleAndNumber(structure as Complex, depth);
+      return (
+        <>
+          {(structure as Complex).children.slice(2).map((child) => (
+            <TableOfContentsGroup structure={child} depth={depth + 1} />
+          ))}
+        </>
+      );
     case Structure.PASAL:
-      return renderPasal(structure as Complex, depth);
     case Structure.LIST:
     case Structure.LIST_ITEM:
     case Structure.PLAINTEXT:
@@ -33,51 +87,31 @@ function renderStructure(
     case Structure.PARAGRAF_NUMBER:
     case Structure.PARAGRAF_TITLE:
     default:
-      return <></>;
+      return null;
   }
 }
 
-function renderUndangUndang(structure: Complex): JSX.Element {
-  return (
-    <>{structure.children.map((children) => renderStructure(children, 0))}</>
-  );
-}
-
-function renderStructuresWithTitleAndNumber(structure: Complex, depth: number) {
-  const number = structure.children[0] as Primitive;
-  const title = structure.children[1] as Primitive;
-  return (
-    <>
-      {renderLink(`${number.text}: ${title.text}`, depth)}
-      {structure.children
-        .slice(2)
-        .map((children) => renderStructure(children, depth + 1))}
-    </>
-  );
-}
-
-function renderPasal(structure: Complex, depth: number) {
-  const number = structure.children[0] as Primitive;
-  return (
-    <>
-      {renderLink(`${number.text}`, depth)}
-      {structure.children
-        .slice(2)
-        .map((children) => renderStructure(children, depth + 1))}
-    </>
-  );
-}
-
-// TODO(johnamadeo): Add link
-function renderLink(label: string, depth: number): JSX.Element {
-  return (
-    <p
-      style={{
-        marginLeft: `${depth * 28}px`,
-        padding: "4px",
-      }}
-    >
-      {label}
-    </p>
-  );
+function getLabel(structure: Complex | Primitive): string | null {
+  switch (structure.type) {
+    case Structure.BAB:
+    case Structure.BAGIAN:
+    case Structure.PARAGRAF:
+      const number = (structure as Complex).children[0] as Primitive;
+      const title = (structure as Complex).children[1] as Primitive;
+      return `${number.text}: ${title.text}`;
+    case Structure.PASAL:
+      return ((structure as Complex).children[0] as Primitive).text;
+    case Structure.LIST:
+    case Structure.LIST_ITEM:
+    case Structure.PLAINTEXT:
+    case Structure.BAB_NUMBER:
+    case Structure.BAB_TITLE:
+    case Structure.PASAL_NUMBER:
+    case Structure.BAGIAN_NUMBER:
+    case Structure.BAGIAN_TITLE:
+    case Structure.PARAGRAF_NUMBER:
+    case Structure.PARAGRAF_TITLE:
+    default:
+      return null;
+  }
 }
