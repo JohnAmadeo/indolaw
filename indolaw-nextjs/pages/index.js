bc44c570f235226b7ba54df275db1d524c65bf40
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-01-01 16:34:57 -0800

    Clean up WIP homepage

--- indolaw-nextjs/pages/index.js
+++ indolaw-nextjs/pages/index.js
@@ -6,26 +6,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>HukumJelas BETA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Go to <Link href="/uu/ciptakerja">law</Link>
+          Go to <Link href="/uu/ciptakerja">demo</Link>
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
