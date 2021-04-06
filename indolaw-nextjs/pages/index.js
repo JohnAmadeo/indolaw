b4665d2c43173eb6686860e95fc5f9566d07ba84
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-04-05 21:40:27 -0700

    [UU] Add UU 1 1974 Tentang Perkawinan to homepage

--- indolaw-nextjs/pages/index.js
+++ indolaw-nextjs/pages/index.js
@@ -26,10 +26,13 @@ export default function Home() {
       <h1>HukumJelas BETA 🚧</h1>
       <p>The search feature is still a work-in-progress. </p>
       <p>For now, below is a list of laws that are available on HukumJelas.</p>
-      <p>Alternatively, if you know a law exists in HukumJelas, you can navigate to its URL directly by using either its a) year and number, or b) nickname. For example, Undang-Undang 13 Tahun 2003 Tentang Ketenagakerjaan will be at <span className="link"><Link href="/uu/2003/13">hukumjelas.com/uu/2003/13</Link></span> and <span className="link"><Link href="/uu/ketenagakerjaan">hukumjelas.com/uu/ketenagakerjaan</Link></span></p>
+      <p>Alternatively, if you know a law exists in HukumJelas, you can navigate to its URL directly by using either its a) year and number, or b) nickname. For example, Undang-Undang 1 Tahun 1974 Tentang Perkawinan will be at <span className="link"><Link href="/uu/1974/1">hukumjelas.com/uu/1974/1</Link></span> and <span className="link"><Link href="/uu/perkawinan">hukumjelas.com/uu/perkawinan</Link></span></p>
       <p>If you <i>also</i> know the exact Bab or Pasal you want to go to, you can also navigate to it directly! For example, Pasal 4 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#pasal-4">hukumjelas.com/uu/2003/13#pasal-4</Link></span> and Bab 10 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#bab-10">hukumjelas.com/uu/2003/13#bab-10</Link></span></p>
       <h3>List of Laws</h3>
       <ul>
+        <li>
+          <span className="link"><Link href="/uu/1974/1">Undang-Undang 1 Tahun 1974 Tentang Perkawinan</Link></span>
+        </li>
         <li>
           <span className="link"><Link href="/uu/2003/13">Undang-Undang 13 Tahun 2003 Tentang Ketenagakerjaan</Link></span>
         </li>
