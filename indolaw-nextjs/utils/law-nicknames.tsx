3069cbe452f604b4e15b9c4ef9e836fedb456ee9
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-04-15 21:44:53 -0700

    Remove UU Cipta Kerja

--- indolaw-nextjs/utils/law-nicknames.tsx
+++ indolaw-nextjs/utils/law-nicknames.tsx
@@ -1,7 +1,6 @@
 export const LAW_NICKNAMES: {
   [index: string]: string,
 } = {
-  'ciptakerja': 'uu-2020-11',
   'ketenagakerjaan': 'uu-2003-13',
   'perlindungankonsumen': 'uu-1999-8',
   'peradilantatausahanegara': 'uu-1986-5',
