924972880afde085054f06bc59ee01b65d1ed89b
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-04-24 20:12:44 -0700

    Fix broken import path

--- indolaw-nextjs/pages/_document.js
+++ indolaw-nextjs/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ContextWrapper } from "../utils/state-management/context-provider";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
