8901a16a22402ca751130fc3f3fd3a163ef2e595
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-06-11 18:25:28 -0700

    [Tray] Add keyboard shortcut component

---
+++ indolaw-nextjs/components/KeyboardShorcut.tsx
@@ -0,0 +1,28 @@
+import { useAppContext } from "utils/context-provider";
+
+export default function KeyboardShortcut(props: {
+  text: string,
+}): JSX.Element {
+  const { colorScheme } = useAppContext();
+
+  return (
+    <div className="keyboard-shortcut">
+      <style jsx>{`
+        .keyboard-shortcut {
+          display: block;
+          background-color: ${colorScheme.tray.button};
+          border-radius: 4px;
+          width: 24px;
+          height: 28px;
+          display: flex;
+          place-items: center center;
+        }
+
+        .keyboard-shortcut-inner {
+          margin: 0 auto;
+        }
+      `}</style>
+      <div className="keyboard-shortcut-inner">{props.text}</div>
+    </div>
+  );
+}
