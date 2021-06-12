84422c3c21af9965187401bf123930b5f32e5537
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-06-11 18:48:24 -0700

    [Tray] Make tray collapsible/expandable

--- indolaw-nextjs/components/LawPage.tsx
+++ indolaw-nextjs/components/LawPage.tsx
@@ -1,86 +1,65 @@
 import { LawData } from "utils/grammar";
 import Law from "components/Law";
-import React from "react";
-import MobileTray from "components/MobileTray";
+import { useState } from "react";
 import Head from "next/head";
 import { useAppContext } from "../utils/context-provider";
-import { useMediaQuery } from "react-responsive";
 import Tray from "./Tray";
 
 // TODO(johnamadeo): Fix "Warning: Each child in a list should have a unique "key" prop." problem
 export default function LawPage(props: {
   law: LawData;
 }): JSX.Element {
-  const border = "2px solid red";
-  const navWidth = "400px";
-
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const [isTrayExpanded, setIsTrayExpanded] = useState(true);
   const { colorScheme } = useAppContext();
 
+  const expandedTrayWidth = 320;
+  // Ideally we'd want to set the width of the minimized tray to just 'inherit' so that it'll be as
+  // wide as its contents; however in order to animate the tray collapsing/expanding we cannot use
+  // 'inherit' due to a CSS transition limitation. See https://css-tricks.com/using-css-transitions-auto-dimensions/
+  const minimizedTrayWidth = 80;
+  const trayWidth = isTrayExpanded ? expandedTrayWidth : minimizedTrayWidth;
+
   return (
-    <div>
+    <div className="container">
+      <style jsx>{`
+        .container {
+          display: flex;
+          height: 100%;
+        }
+
+        .law-container {
+          flex-grow: 1;
+          max-width: calc(100% - ${trayWidth}px);
+          transition: max-width ease 0.2s;
+          background-color: ${colorScheme.background};
+        }
+
+        .law {
+          margin: 0 auto;
+          width: 768px;
+        }
+
+        @media screen and (max-width: 1224px) {
+          .law {
+            width: auto;
+            padding: 0 24px;
+          }
+        }
+      `}</style>
       <Head>
         <title>UU No. {props.law.metadata.number} Tahun {props.law.metadata.year}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="table-of-contents-container">
-        <style jsx>{`
-          .table-of-contents-container {
-            height: 100%;
-            overflow: auto;
-            position: fixed;
-            padding: 20px;
-            width: ${navWidth};
-            background-color: ${colorScheme.tray.background};
-          }
-
-          @media screen and (max-width: 768px) {
-            .table-of-contents-container {
-              background-color: ${colorScheme.tray.background};
-              top: 0;
-              height: 60px;
-              overflow: hidden;
-              width: 100vw;
-              z-index: 1;
-            }
-          }
-        `}</style>
-        {isMobile ? <MobileTray law={props.law} /> : <Tray law={props.law} />}
-      </div>
+      <Tray
+        law={props.law}
+        isExpanded={isTrayExpanded}
+        onExpand={() => setIsTrayExpanded(true)}
+        onMinimize={() => setIsTrayExpanded(false)}
+        width={trayWidth}
+      />
 
       <div className="law-container">
-        <style jsx>{`
-          .law-container {
-            background-color: ${colorScheme.background};
-            position: absolute;
-            left: ${navWidth};
-            right: 0;
-          }
 
-          .law {
-            margin: 0 auto;
-            width: 768px;
-          }
-
-          @media screen and (max-width: 1224px) {
-            .law {
-              width: auto;
-              padding: 0 24px;
-            }
-          }
-
-          @media screen and (max-width: 768px) {
-            .law-container {
-              position: absolute;
-              top: 60px;
-              left: 0;
-              overflow: scroll;
-              height: calc(100% - 80px);
-              font-size: 14px;
-              // border: ${border};
-            }
-          }
-        `}</style>
         <div className="law">
           <Law law={props.law.content} colorScheme={colorScheme} />
         </div>
