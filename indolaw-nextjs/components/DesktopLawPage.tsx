c6fb8284bff45d2a4192e12c8c4a4d18e1332da8
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2021-06-11 20:02:43 -0700

    Add back mobile UI

---
+++ indolaw-nextjs/components/DesktopLawPage.tsx
@@ -0,0 +1,65 @@
+import { LawData } from "utils/grammar";
+import Law from "components/Law";
+import { useState } from "react";
+import Head from "next/head";
+import { useAppContext } from "../utils/context-provider";
+import Tray from "./Tray";
+
+// TODO(johnamadeo): Fix "Warning: Each child in a list should have a unique "key" prop." problem
+export default function DesktopLawPage(props: {
+  law: LawData;
+}): JSX.Element {
+  const [isTrayExpanded, setIsTrayExpanded] = useState(true);
+  const { colorScheme } = useAppContext();
+
+  const expandedTrayWidth = 320;
+  // Ideally we'd want to set the width of the minimized tray to just 'inherit' so that it'll be as
+  // wide as its contents; however in order to animate the tray collapsing/expanding we cannot use
+  // 'inherit' due to a CSS transition limitation. See https://css-tricks.com/using-css-transitions-auto-dimensions/
+  const minimizedTrayWidth = 76;
+  const trayWidth = isTrayExpanded ? expandedTrayWidth : minimizedTrayWidth;
+
+  return (
+    <div className="container">
+      <style jsx>{`
+        .container {
+          display: flex;
+          height: 100%;
+        }
+
+        .law-container {
+          flex-grow: 1;
+          max-width: calc(100% - ${trayWidth}px);
+          transition: max-width ease 0.2s;
+          background-color: ${colorScheme.background};
+        }
+
+        .law {
+          margin: 0 auto;
+          width: 768px;
+        }
+
+        @media screen and (max-width: 1224px) {
+          .law {
+            width: auto;
+            padding: 0 24px;
+          }
+        }
+      `}</style>
+      <Tray
+        law={props.law}
+        isExpanded={isTrayExpanded}
+        onExpand={() => setIsTrayExpanded(true)}
+        onMinimize={() => setIsTrayExpanded(false)}
+        width={trayWidth}
+      />
+
+      <div className="law-container">
+
+        <div className="law">
+          <Law law={props.law.content} colorScheme={colorScheme} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- indolaw-nextjs/components/LawPage.tsx
+++ indolaw-nextjs/components/LawPage.tsx
@@ -1,69 +1,24 @@
 import { LawData } from "utils/grammar";
-import Law from "components/Law";
-import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import Head from "next/head";
-import { useAppContext } from "../utils/context-provider";
-import Tray from "./Tray";
+
+import DesktopLawPage from "./DesktopLawPage";
+import MobileLawPage from "./MobileLawPage";
 
 // TODO(johnamadeo): Fix "Warning: Each child in a list should have a unique "key" prop." problem
 export default function LawPage(props: {
   law: LawData;
 }): JSX.Element {
-  const [isTrayExpanded, setIsTrayExpanded] = useState(true);
-  const { colorScheme } = useAppContext();
-
-  const expandedTrayWidth = 320;
-  // Ideally we'd want to set the width of the minimized tray to just 'inherit' so that it'll be as
-  // wide as its contents; however in order to animate the tray collapsing/expanding we cannot use
-  // 'inherit' due to a CSS transition limitation. See https://css-tricks.com/using-css-transitions-auto-dimensions/
-  const minimizedTrayWidth = 80;
-  const trayWidth = isTrayExpanded ? expandedTrayWidth : minimizedTrayWidth;
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const { law } = props;
 
   return (
-    <div className="container">
-      <style jsx>{`
-        .container {
-          display: flex;
-          height: 100%;
-        }
-
-        .law-container {
-          flex-grow: 1;
-          max-width: calc(100% - ${trayWidth}px);
-          transition: max-width ease 0.2s;
-          background-color: ${colorScheme.background};
-        }
-
-        .law {
-          margin: 0 auto;
-          width: 768px;
-        }
-
-        @media screen and (max-width: 1224px) {
-          .law {
-            width: auto;
-            padding: 0 24px;
-          }
-        }
-      `}</style>
+    <div>
       <Head>
-        <title>UU No. {props.law.metadata.number} Tahun {props.law.metadata.year}</title>
+        <title>UU No. {law.metadata.number} Tahun {law.metadata.year}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Tray
-        law={props.law}
-        isExpanded={isTrayExpanded}
-        onExpand={() => setIsTrayExpanded(true)}
-        onMinimize={() => setIsTrayExpanded(false)}
-        width={trayWidth}
-      />
-
-      <div className="law-container">
-
-        <div className="law">
-          <Law law={props.law.content} colorScheme={colorScheme} />
-        </div>
-      </div>
+      {isMobile ? <MobileLawPage law={law} /> : <DesktopLawPage law={law} />}
     </div>
   );
 }
