eebe71df8c0e06c686944fb675e963d3f3299896
willem.chua <willem@xendit.co>
2021-07-29 05:35:47 +0700

    [Homepage]: Add selectable year list on top of law list

--- indolaw-nextjs/pages/index.js
+++ indolaw-nextjs/pages/index.js
@@ -1,11 +1,27 @@
 import Head from "next/head";
 import Link from "next/link";
 import fs from "fs";
-import { GetStaticProps } from "next";
-import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
+// TODO willem:
+// 1. Search bar (fuzzy search + uu number search)
 export default function Home(props) {
   const { laws } = props.data;
+  const ALL_YEAR_STRING = "Show All";
+
+  const [selectedYear, setSelectedYear] = useState(ALL_YEAR_STRING);
+  const [selectedLaws, setSelectedLaws] = useState(laws);
+
+  const yearList = [ALL_YEAR_STRING, ...Object.keys(laws).sort()];
+
+  const onYearChange = (year) => {
+    if (year === selectedYear) {
+      return;
+    }
+
+    setSelectedYear(year);
+    setSelectedLaws(year === ALL_YEAR_STRING ? laws : { [year]: laws[year] });
+  };
 
   return (
     <div className="container">
@@ -27,6 +43,21 @@ export default function Home(props) {
         .link:hover {
           text-decoration: underline;
         }
+
+        #selected {
+          text-decoration: none;
+          cursor: default;
+          color: black;
+        }
+
+        ul#year-list {
+          list-style-type: none;
+        }
+
+        ul#year-list li {
+          display: inline-block;
+          margin: 10px;
+        }
       `}</style>
       <Head>
         <title>HukumJelas BETA 🚧</title>
@@ -39,57 +70,74 @@ export default function Home(props) {
       <p>If you <i>also</i> know the exact Bab or Pasal you want to go to, you can also navigate to it directly! For example, Pasal 4 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#pasal-4">hukumjelas.com/uu/2003/13#pasal-4</Link></span> and Bab 10 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#bab-10">hukumjelas.com/uu/2003/13#bab-10</Link></span></p>
       <h3>List of Laws</h3>
 
-      <ul>
-        {Object.keys(laws).sort().map(year => (
+      <ul id="year-list">
+        {yearList.map((year) => (
           <li>
-            <p>{year}</p>
-            <ul>
-              {laws[year]
-                .sort((a, b) => {
-                  if (a.number > b.number) {
-                    return 1;
-                  } else if (a.number === b.number) {
-                    return 0;
-                  } else {
-                    return -1;
-                  }
-                })
-                .map(data => {
-                  return (
-                    <li>
-                      <p>
-                        <Link href={`/uu/${year}/${data.number}`}>
-                          <span className="link">
-                            Undang-Undang {data.number} Tahun {year} Tentang {data.topic}
-                          </span>
-                        </Link>
-                      </p>
-                    </li>
-                  );
-                })}
-            </ul>
+            <span
+              className="link"
+              id={year === selectedYear ? "selected" : ""}
+              onClick={() => onYearChange(year)}
+            >
+              {year}
+            </span>
           </li>
         ))}
+      </ul>
+
+      <ul>
+        {Object.keys(selectedLaws)
+          .sort()
+          .map((year) => (
+            <li>
+              <p>{year}</p>
+              <ul>
+                {laws[year]
+                  .sort((a, b) => {
+                    if (a.number > b.number) {
+                      return 1;
+                    } else if (a.number === b.number) {
+                      return 0;
+                    } else {
+                      return -1;
+                    }
+                  })
+                  .map((data) => {
+                    return (
+                      <li>
+                        <p>
+                          <a href={data.link}>
+                            <span className="link">
+                              Undang-Undang {data.number} Tahun {data.year} Tentang {data.topic}
+                            </span>
+                          </a>
+                        </p>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </li>
+          ))}
       </ul>
     </div>
   );
 }
 
 export const getStaticProps = async ({ params }) => {
-  const lawsList = fs.readdirSync('./laws')
-    .map(filename => {
-      const parts = filename.replace(/\.json$/, '').split('-');
-      return {
-        year: parts[1],
-        number: parts[2],
-      };
-    });
+  const lawsList = fs.readdirSync("./laws").map((filename) => {
+    const parts = filename.replace(/\.json$/, "").split("-");
+    return {
+      year: parts[1],
+      number: parts[2],
+    };
+  });
 
   const laws = {};
   for (let law of lawsList) {
     const year = parseInt(law.year);
 
-    const file = JSON.parse(fs.readFileSync(`./laws/uu-${law.year}-${law.number}.json`, 'utf8'));
+    const file = JSON.parse(
+      fs.readFileSync(`./laws/uu-${law.year}-${law.number}.json`, "utf8")
+    );
 
     if (!(year in laws)) {
       laws[year] = [];
@@ -98,6 +146,8 @@ export const getStaticProps = async ({ params }) => {
     laws[year].push({
       number: parseInt(law.number),
       topic: file.metadata.topic,
+      year: year,
+      link: `/uu/${year}/${law.number}`,
     });
   }
 
