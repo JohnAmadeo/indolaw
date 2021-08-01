9dce31958016319f396d7251e14508b1fb87eb16
John <john.a.daniswara@gmail.com>
2021-07-31 17:46:39 -0700

    Test device detection library

--- indolaw-nextjs/components/LawPage.tsx
+++ indolaw-nextjs/components/LawPage.tsx
@@ -1,16 +1,17 @@
 import { LawData } from "utils/grammar";
 import { useMediaQuery } from "react-responsive";
 import Head from "next/head";
+import { isMobile } from 'react-device-detect';
 
 import DesktopLawPage from "./DesktopLawPage";
 import MobileLawPage from "./MobileLawPage";
 
 // TODO(johnamadeo): Fix "Warning: Each child in a list should have a unique "key" prop." problem
 export default function LawPage(props: { law: LawData }): JSX.Element {
-  const border = "2px solid red";
-  const navWidth = "400px";
+  const isMobileSize = useMediaQuery({ query: "(max-width: 768px)" });
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
 
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   const { law } = props;
 
   return (
@@ -19,7 +20,9 @@ export default function LawPage(props: { law: LawData }): JSX.Element {
         <title>UU No. {law.metadata.number} Tahun {law.metadata.year}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {isMobile ? <MobileLawPage law={law} /> : <DesktopLawPage law={law} />}
+      <h1>react-device-detect: {isMobile ? 'mobile or tablet' : 'desktop'}</h1>
+      <h1>{isPortrait ? 'portrait' : 'landscape'}</h1>
+      {/* {isMobileSize ? <MobileLawPage law={law} /> : <DesktopLawPage law={law} />} */}
     </div>
   );
 }
