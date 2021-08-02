916d77e07fcfe22607fa1cd37be45269941f78da
willem.chua <willem@xendit.co>
2021-08-02 07:18:33 +0700

    Homepage: Add search bar

--- indolaw-nextjs/pages/index.js
+++ indolaw-nextjs/pages/index.js
@@ -2,17 +2,23 @@ import Head from "next/head";
 import Link from "next/link";
 import fs from "fs";
 import { useState } from "react";
+import _ from 'lodash';
+import Fuse from 'fuse.js';
 
-// TODO willem:
-// 1. Search bar (fuzzy search + uu number search)
 export default function Home(props) {
   const { laws } = props.data;
   const ALL_YEAR_STRING = "Show All";
 
   const [selectedYear, setSelectedYear] = useState(ALL_YEAR_STRING);
-  const [selectedLaws, setSelectedLaws] = useState(laws);
+  const [isSearching, setIsSearching] = useState(false);
+  const [selectedLaws, setSelectedLaws] = useState([]);
 
   const yearList = [ALL_YEAR_STRING, ...Object.keys(laws).sort()];
+
+  const fuse = new Fuse(_.flatMap(_.values(laws)), {
+    keys: ['link', 'topic', 'number', 'year'],
+    shouldSort: true
+  });
 
   const onYearChange = (year) => {
     if (year === selectedYear) {
@@ -20,8 +26,91 @@ export default function Home(props) {
     }
 
     setSelectedYear(year);
-    setSelectedLaws(year === ALL_YEAR_STRING ? laws : { [year]: laws[year] });
   };
+
+  const onSearch = (e) => {
+    if (_.isEmpty(e)) {
+      resetSearch();
+      return;
+    }
+
+    setIsSearching(true);
+
+    const searchResult = fuse.search(e, {
+      limit: 20
+    });
+
+    setSelectedLaws(searchResult.map(res => res.item));
+  }
+
+  const resetSearch = () => {
+    setIsSearching(false);
+    setSelectedLaws([]);
+  }
+
+  const styles = (
+    <style jsx>{`
+    p {
+      margin: 12px 0;
+    }
+
+    .link {
+      cursor: pointer;
+      color: blue;
+    }
+
+    .link:hover {
+      text-decoration: underline;
+    }
+  `}</style>
+  );
+
+  const renderLawListItem = (item) => {
+    return (<>
+    {styles}
+      <li>
+        <p>
+          <a href={item.link}>
+            <span className="link">
+              Undang-Undang {item.number} Tahun {item.year} Tentang {item.topic}
+            </span>
+          </a>
+        </p>
+      </li>
+      </>
+    );
+  }
+
+  const allLawsDisplay = (
+    <>
+      {styles}
+      <ul>
+        {Object.keys(
+          selectedYear === ALL_YEAR_STRING
+            ? laws
+            : { [selectedYear]: laws[selectedYear] }
+        )
+          .sort()
+          .map((year) => (
+            <li>
+              <p>{year}</p>
+              <ul>
+                {laws[year]
+                  .sort((a, b) => parseInt(a.number) - parseInt(b.number))
+                  .map(renderLawListItem)}
+              </ul>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+
+  const searchResult = (
+    <>
+      {styles}
+      <ul>{selectedLaws.map(renderLawListItem)}</ul>
+    </>
+  );
 
   return (
     <div className="container">
@@ -64,60 +153,55 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>HukumJelas BETA 🚧</h1>
-      <p>The search feature is still a work-in-progress. </p>
-      <p>For now, below is a list of laws that are available on HukumJelas.</p>
-      <p>Alternatively, if you know a law exists in HukumJelas, you can navigate to its URL directly by using either its a) year and number, or b) nickname. For example, Undang-Undang 1 Tahun 1974 Tentang Perkawinan will be at <span className="link"><Link href="/uu/1974/1">hukumjelas.com/uu/1974/1</Link></span> and <span className="link"><Link href="/uu/perkawinan">hukumjelas.com/uu/perkawinan</Link></span></p>
-      <p>If you <i>also</i> know the exact Bab or Pasal you want to go to, you can also navigate to it directly! For example, Pasal 4 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#pasal-4">hukumjelas.com/uu/2003/13#pasal-4</Link></span> and Bab 10 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#bab-10">hukumjelas.com/uu/2003/13#bab-10</Link></span></p>
+      <p>This page displays a list of laws that are available on HukumJelas.</p>
+      <p>
+        Alternatively, if you know a law exists in HukumJelas, you can navigate
+        to its URL directly by using either its a) year and number, or b)
+        nickname. For example, Undang-Undang 1 Tahun 1974 Tentang Perkawinan
+        will be at <span className="link"><Link href="/uu/1974/1">hukumjelas.com/uu/1974/1</Link></span>
+        and <span className="link"><Link href="/uu/perkawinan">hukumjelas.com/uu/perkawinan</Link></span>
+      </p>
+      <p>
+        If you <i>also</i> know the exact Bab or Pasal you want to go to, you
+        can also navigate to it directly! For example, Pasal 4 of UU 13 2003 is
+        at<span className="link"><Link href="/uu/2003/13#pasal-4">hukumjelas.com/uu/2003/13#pasal-4</Link></span>
+        and Bab 10 of UU 13 2003 is at <span className="link"><Link href="/uu/2003/13#bab-10">hukumjelas.com/uu/2003/13#bab-10</Link></span>
+      </p>
+      <p>
+        The search feature is still a work-in-progress. For now, if you want to
+        get the best approximation for the search result, you can try to search
+        the topics of the UU (ex: "Perkawinan", "Kawin", etc) or search the year
+        and number of UU with the format "year/number" (ex: "1997/8", "2002/2",
+        etc)
+      </p>
+
+      <input
+        className="search-box"
+        onKeyDown={_.debounce((e) => onSearch(e.target.value), 150)}
+        placeholder="Search..."
+      ></input>
+
       <h3>List of Laws</h3>
 
-      <ul id="year-list">
-        {yearList.map((year) => (
-          <li>
-            <span
-              className="link"
-              id={year === selectedYear ? "selected" : ""}
-              onClick={() => onYearChange(year)}
-            >
-              {year}
-            </span>
-          </li>
-        ))}
-      </ul>
-
-      <ul>
-        {Object.keys(selectedLaws)
-          .sort()
-          .map((year) => (
+      {!isSearching && (
+        <ul id="year-list">
+          {yearList.map((year) => (
             <li>
-              <p>{year}</p>
-              <ul>
-                {laws[year]
-                  .sort((a, b) => {
-                    if (a.number > b.number) {
-                      return 1;
-                    } else if (a.number === b.number) {
-                      return 0;
-                    } else {
-                      return -1;
-                    }
-                  })
-                  .map((data) => {
-                    return (
-                      <li>
-                        <p>
-                          <a href={data.link}>
-                            <span className="link">
-                              Undang-Undang {data.number} Tahun {data.year} Tentang {data.topic}
-                            </span>
-                          </a>
-                        </p>
-                      </li>
-                    );
-                  })}
-              </ul>
+              <span
+                className="link"
+                id={year === selectedYear ? "selected" : ""}
+                onClick={() => onYearChange(year)}
+              >
+                {year}
+              </span>
             </li>
           ))}
-      </ul>
+        </ul>
+      )}
+
+      {isSearching ? searchResult : allLawsDisplay}
+
+      {isSearching && selectedLaws.length == 0 && <p>Sorry, but it looks like nothing comes up with that search query</p>}
     </div>
   );
 }
