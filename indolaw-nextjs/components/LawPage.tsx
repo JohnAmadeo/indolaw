87b6fa64b72376d042422cf5810872a0e9b14e37
John <john.a.daniswara@gmail.com>
2021-08-08 22:46:39 -0700

    test

--- indolaw-nextjs/components/LawPage.tsx
+++ indolaw-nextjs/components/LawPage.tsx
@@ -4,22 +4,22 @@ import dynamic from 'next/dynamic'
 
 import DesktopLawPage from "./DesktopLawPage";
 import MobileLawPage from "./MobileLawPage";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // TODO(johnamadeo): Fix "Warning: Each child in a list should have a unique "key" prop." problem
 export default function LawPage(props: { law: LawData }): JSX.Element {
   const { law } = props;
 
-  const isMobileRef = useRef(false);
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
 
   useEffect(() => {
     async function detectDevice() {
       const { isMobile } = await import('react-device-detect');
-      isMobileRef.current = isMobile;
+      setIsMobileDevice(isMobile);
     }
 
     detectDevice();
-  }, [isMobileRef]);
+  }, [setIsMobileDevice]);
 
   return (
     <div>
@@ -27,8 +27,8 @@ export default function LawPage(props: { law: LawData }): JSX.Element {
         <title>UU No. {law.metadata.number} Tahun {law.metadata.year}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>{isMobileRef.current ? 'isMobile' : 'NOT isMobile'}</h1>
-      {isMobileRef.current ? <MobileLawPage law={law} /> : <DesktopLawPage law={law} />}
+      <h1>{isMobileDevice ? 'isMobile' : 'NOT isMobile'}</h1>
+      {isMobileDevice ? <MobileLawPage law={law} /> : <DesktopLawPage law={law} />}
     </div>
   );
 }
